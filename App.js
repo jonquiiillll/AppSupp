b4cd252280bcf316acef83d7b0a5f51f69fa0f54
jonquiiillll <71707079+jonquiiillll@users.noreply.github.com>
2022-12-21 23:30:09 +0300

    New

--- App.js
+++ App.js
@@ -1,92 +1,10 @@
 import { StyleSheet, ActivityIndicator, Card, TouchableOpacity, Buttton, Image, FlatList, Text, View, } from 'react-native'
 import React, { useState, useEffect} from "react"
 const apiUrl = 'http://localhost:3000/api/v1/users'
-import {NavigationContainer} from '@react-navigation/native';
+import MainStack from './screens/Main'
 
 export default App = () => {
-  const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])
-
-  const getData = async () => {
-    try {
-      const response = await fetch(
-        apiUrl,
-        {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
-        }
-      )
-
-      const json = await response.json()
-      setData(json.data)
-    } catch (error) {
-        console.error(error)
-    } finally {
-        setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-    {
     return (
-      <View
-      data={data}
-      style={styles.container}>
-        <Text>Team</Text>
-        <Text>{data.name}</Text>
-        {isLoading ? <ActivityIndicator/> : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }) => id}
-            renderItem={({ item }) => (
-            <TouchableOpacity onPress={ () => navigation.navigate('FullAccountView')}>
-              <View style={styles.card}>
-              <View style={styles.userName}>
-                <Image style={{height:105,width:105,  borderRadius: 100,}} source={{uri: item.avatar}} />
-                <Text style={{fontSize:36, marginLeft:15,}}>{item.name}</Text>
-              </View>
-              <Text style={{fontSize:25, fontWeight: 'semibold', marginTop: 25, marginBottom: 10,}}>Обо мне</Text>
-              <Text style={{fontSize:17, flex:1}}>{item.description}</Text>
-              <Text style={{fontSize:17, flex:1}}>{item.problems}</Text>
-
-              </View>
-            </TouchableOpacity >
-            )}
-          />
-        )}
-      </View>
-
+      <Main/>
     )
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingLeft: 16,
-    paddingRight: 16,
-    backgroundColor: '#F0F0F0',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 30,
-  },
-  card: {
-
-    backgroundColor: 'white',
-    width: '100%',
-    justifyContent: 'center',
-    marginTop: 30,
-    borderRadius: 20,
-    padding: 20,
-  },
-  avatar: {
-    width: 40,
-    hight: 40,
-    resizeMode: 'contain',
-  },
-  userName: {
-    flexDirection: 'row',
-  },
-})
--- screens/FullAccountView.js
+++ screens/FullAccountView.js
@@ -1,14 +1,12 @@
 import { StyleSheet, ActivityIndicator, Card, Buttton, Image, FlatList, Text, View } from 'react-native'
 import React, { PureComponent } from "react"
 
-class FullAccountView extends PureComponent {
-  render() {
-    return (
-      <Text>
-      Component
-      </Text>
-    )
-  }
-}
 
-export default FullAccountView
+
+export default function FullAccountView () {
+  return (
+    <Text>
+    Component
+    </Text>
+ )
+}
