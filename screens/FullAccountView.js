ef83c87c1ca5f315f447f7857752931f3095b737
jonquiiillll <71707079+jonquiiillll@users.noreply.github.com>
2022-12-21 20:45:44 +0300

    init commit

---
+++ screens/FullAccountView.js
@@ -0,0 +1,14 @@
+import { StyleSheet, ActivityIndicator, Card, Buttton, Image, FlatList, Text, View } from 'react-native'
+import React, { PureComponent } from "react"
+
+class FullAccountView extends PureComponent {
+  render() {
+    return (
+      <Text>
+      Component
+      </Text>
+    )
+  }
+}
+
+export default FullAccountView
